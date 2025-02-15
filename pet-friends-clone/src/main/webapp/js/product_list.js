81d3dcb35076073f5f0bc5b9762042f677943577
devcube2 <jejeje8@knou.ac.kr>
2025-02-15 20:57:04 +0900

    refactor: 상품조회 프론트 구현 수정

--- pet-friends-clone/src/main/webapp/js/product_list.js
+++ pet-friends-clone/src/main/webapp/js/product_list.js
@@ -8,32 +8,27 @@ const getProductList = async () => {
 		const response = await fetch('/pet-friends-clone/products', option);
 		const dtoList = await response.json();
 
+        // {
+        //     "name": "알모네이쳐 캣 대서양참치 캔 70g",
+        //     "price": 2600,
+        //     "petType": "고양이",
+        //     "petCareItemCategory1": "사료",
+        //     "petCareItemCategory2": 5,
+        //     "petCareItemCategory2s": "주식캔",
+        //     "attributes": "",
+        //     "stockQuantity": 0,
+        //     "discount": 0,
+        //     "contents": "{\"slideList\":[\"slide1.webp\",\"slide2.jpeg\",\"slide3.jpeg\",\"slide4.jpeg\"], \"mainList\":[\"main1.webp\",\"main2.webp\",\"main3.webp\",\"main4.webp\",\"main5.webp\",\"main6.webp\",\"main7.webp\"]}"
+        //     },
+
 		dtoList.forEach(dto => {
 			html += `
-				    <li class="width190 margin-bottom12">
-                        <img src="resources/images/products/고양이/사료/주식캔/1/slide/slide1.webp" class="w190h190 bd-radius12">
-                        <p class="font-13 letter-minus1">${dto.name}</p>
-                        <p class="pad5-0-2"><span class="bold">${dto.price}</span><span class="font-15 pos-bottom1">원</span></p>
-                        <img src="resources/images/products/badge-morning.png" class="w62h16">
-                    </li>
-                    <li class="width190 margin-bottom12">
-                        <img src="resources/images/products/고양이/사료/주식캔/1/slide/slide1.webp" class="w190h190 bd-radius12">
-                        <p class="font-13 letter-minus1">${dto.name}</p>
-                        <p class="pad5-0-2"><span class="bold">${dto.price}</span><span class="font-15 pos-bottom1">원</span></p>
-                        <img src="resources/images/products/badge-morning.png" class="w62h16">
-                    </li>
-                    <li class="width190 margin-bottom12">
-                        <img src="resources/images/products/고양이/사료/주식캔/1/slide/slide1.webp" class="w190h190 bd-radius12">
-                        <p class="font-13 letter-minus1">${dto.name}</p>
-                        <p class="pad5-0-2"><span class="bold">${dto.price}</span><span class="font-15 pos-bottom1">원</span></p>
-                        <img src="resources/images/products/badge-morning.png" class="w62h16">
-                    </li>
-                    <li class="width190 margin-bottom12">
-                        <img src="resources/images/products/고양이/사료/주식캔/1/slide/slide1.webp" class="w190h190 bd-radius12">
-                        <p class="font-13 letter-minus1">${dto.name}</p>
-                        <p class="pad5-0-2"><span class="bold">${dto.price}</span><span class="font-15 pos-bottom1">원</span></p>
-                        <img src="resources/images/products/badge-morning.png" class="w62h16">
-                    </li>
+                <li class="width190 margin-bottom12">
+                    <img src="resources/images/products/${dto.petType}/${dto.petCareItemCategory1}/${dto.petCareItemCategory2s}/${dto.id}/slide/slide1.webp" class="w190h190 bd-radius12">
+                    <p class="font-13 letter-minus1">${dto.name}</p>
+                    <p class="pad5-0-2"><span class="bold">${dto.price}</span><span class="font-15 pos-bottom1">원</span></p>
+                    <img src="resources/images/products/badge-morning.png" class="w62h16">
+                </li>
 			`;
 		});
 	} catch (error) {
